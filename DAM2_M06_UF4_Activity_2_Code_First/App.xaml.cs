e96b86c913ee86c1bdf556a103ddc6daccd73097
Oleg <kharenkoleg@gmail.com>
2024-02-22 16:42:18 +0100

    Agregar archivos de proyecto.

---
+++ DAM2_M06_UF4_Activity_2_Code_First/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DAM2_M06_UF4_Activity_2_Code_First
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ DAM2_M06_UF4_Activity_2_Code_First/DAO/IDAOManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAM2_M06_UF4_Activity_2_Code_First.DAO
+{
+    public interface IDAOManager
+    {
+        // TODO
+    }
+}
