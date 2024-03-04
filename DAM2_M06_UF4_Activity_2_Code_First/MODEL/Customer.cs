7c6c1ce5b383c522b5e7ab3db362d555c283c84c
Oleg <kharenkoleg@gmail.com>
2024-03-04 18:46:14 +0100

    << lolaso

---
+++ DAM2_M06_UF4_Activity_2_Code_First/MODEL/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAM2_M06_UF4_Activity_2_Code_First.MODEL
+{
+    public class Customer
+    {
+        private int CustomerNumber {  get; set; }
+        private string CustomerName { get; set; }
+        private string ContactLastName { get; set; }
+        private string ContactFirstName { get; set; }
+        private string Phone {  get; set; }
+        private string AdressLine1 { get; set; }
+        private string AdressLine2 { get; set; }
+        private string State { get; set; }
+        private string PostalCode { get; set; }
+        private string Country { get; set; }
+
+
+
+    }
+}
