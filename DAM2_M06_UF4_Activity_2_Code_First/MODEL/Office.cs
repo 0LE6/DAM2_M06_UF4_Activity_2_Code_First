2925817106e4662bacc1acd9b0f6582b29be8ad3
Oleg <kharenkoleg@gmail.com>
2024-03-04 19:07:59 +0100

    add office

---
+++ DAM2_M06_UF4_Activity_2_Code_First/MODEL/Office.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAM2_M06_UF4_Activity_2_Code_First.MODEL
+{
+    public class Office
+    {
+        // TABLE Offices
+        // TODO : Foreign Key que apunta a OfficeCode de la TABLE Employees
+        // private string OfficeCode { get; set; }
+
+        private string City {  get; set; }
+        private string Phone { get; set; }
+        private string AddressLine1 { get; set; }
+        private string AddressLine2 { get; set; }
+        private string State { get; set; }
+        private string Country { get; set; }
+        private string PostalCode { get; set; }
+        private string Territory { get; set; }
+    }
+}
