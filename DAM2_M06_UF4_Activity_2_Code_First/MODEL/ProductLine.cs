f11da784610d22e2e2db371677f81a1d2b9e3e01
Edu <pewpet@gmail.com>
2024-03-04 19:11:18 +0100

    Cambio de nombre de clase: Product Line -> Product Lines

--- DAM2_M06_UF4_Activity_2_Code_First/MODEL/ProductLine.cs
+++ DAM2_M06_UF4_Activity_2_Code_First/MODEL/ProductLine.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DAM2_M06_UF4_Activity_2_Code_First.MODEL
 {
-    public class ProductLine
+    public class ProductLines
     {
         public string ProductLine { get; set; }
         public string TextDescription { get; set; }
