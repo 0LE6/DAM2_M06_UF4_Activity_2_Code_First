bd9cf29cff592d4b7e1c984a2f1537046c0f3bf5
Edu <pewpet@gmail.com>
2024-03-05 21:59:40 +0100

    puliendo models

--- DAM2_M06_UF4_Activity_2_Code_First/MODEL/ProductLine.cs
+++ DAM2_M06_UF4_Activity_2_Code_First/MODEL/ProductLine.cs
@@ -12,5 +12,7 @@ namespace DAM2_M06_UF4_Activity_2_Code_First.MODEL
         public string TextDescription { get; set; }
         public string HtmlDescription { get; set; }
         public string Image { get; set; } // set as string
+
+        public ICollection<Product> Products { get; set; }
     }
 }
