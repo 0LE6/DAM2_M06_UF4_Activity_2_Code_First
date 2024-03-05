01cbf36352e2648f39484eca07672290f01810f0
Oleg <kharenkoleg@gmail.com>
2024-03-05 21:16:41 +0100

    add -- dbcontext

---
+++ DAM2_M06_UF4_Activity_2_Code_First/MODEL/ClassicModelDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAM2_M06_UF4_Activity_2_Code_First.MODEL
+{
+    public class ClassicModelDbContext : DbContext
+    {
+        public ClassicModelDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseMySql(
+                    "Server=localhost;Database=0classicmodel;Uid=root;Pwd=\"\"");
+            }
+        }
+    }
+}
