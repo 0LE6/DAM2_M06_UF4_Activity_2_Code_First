47dd63ab842dd5fce7e517f9fb28e0261842d2a2
Edu <pewpet@gmail.com>
2024-03-05 22:44:59 +0100

    Primera migración, Product tiene Key, ProductLines también y en principio funciona

--- DAM2_M06_UF4_Activity_2_Code_First/MODEL/Product.cs
+++ DAM2_M06_UF4_Activity_2_Code_First/MODEL/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,16 +9,16 @@ namespace DAM2_M06_UF4_Activity_2_Code_First.MODEL
 {
     public class Product
     {
+        [Key]
         public string ProductCode { get; set; }
         public string ProductName { get; set; }
-        //public ProductLines ProductLine { get; set; }
+        public ProductLines ProductLine { get; set; }
         public string ProductScale { get; set; }
         public string ProductVendor { get; set; }
         public string ProductDescription { get; set; }
         public int QuantityInStock { get; set; }
         public decimal BuyPrice { get; set; }
         public decimal MSRP { get; set; }
-
         //public  ICollection<OrderDetail> OrderDetails { get; set; }
     }
 }
--- DAM2_M06_UF4_Activity_2_Code_First/MODEL/ProductLine.cs
+++ DAM2_M06_UF4_Activity_2_Code_First/MODEL/ProductLine.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ namespace DAM2_M06_UF4_Activity_2_Code_First.MODEL
         {
             Products = new HashSet<Product>();
         }
+        [Key]
         public string ProductLine { get; set; }
         public string TextDescription { get; set; }
         public string HtmlDescription { get; set; }
